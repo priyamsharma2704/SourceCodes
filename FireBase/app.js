f998734192756cd4852a9f1560f3e49acb477686
priyamsharma2704 <priyamsahrma2704@gmail.com>
2018-08-03 09:31:09 -0400

    delete function implemented

--- FireBase/app.js
+++ FireBase/app.js
@@ -6,15 +6,25 @@ function renderCafe(doc)
   let li = document.createElement('li');
   let name = document.createElement('span');
   let city = document.createElement('span');
-
+  let cross = document.createElement('div');
+  
   li.setAttribute('data-id',doc.id);
   name.textContent = doc.data().Name;
   city.textContent = doc.data().City;
+  cross.textContent = 'x';
 
   li.appendChild(name);
   li.appendChild(city);
-console.log(li);
+  li.appendChild(cross);
+
   cafeList.appendChild(li);
+
+  //deleting data
+  cross.addEventListener('click', (e)=>{
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute('data-id');
+    db.collection('cafes').doc(id).delete();
+  })
 }
 
 //getting datas
