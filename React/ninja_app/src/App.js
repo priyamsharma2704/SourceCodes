e7455438e77aea1feab6cfe78c57a2c6b421fadb
priyamsharma2704 <priyamsahrma2704@gmail.com>
2018-08-13 21:34:18 -0400

    REACT: passing props

--- React/ninja_app/src/App.js
+++ React/ninja_app/src/App.js
@@ -4,13 +4,19 @@ import Ninjas from './Ninjas';
 //import './App.css';
 
 class App extends Component {
+  state={
+    ninjas:[
+      { name:"ken", age:20, belt:"black", id:1},
+      { name:"ryu", age:40, belt:"brown", id:2},
+      { name:"chun li", age:30, belt:"red", id:3}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Header!!!!</h1>
         <p>Welcome</p>
-        <Ninjas name="Ryu" age="30" belt="Black"/>
-        <Ninjas name="Ken" age="32" belt="Brown"/>
+        <Ninjas ninjas={this.state.ninjas}/>
       </div>
     );
   }
--- React/ninja_app/src/Ninjas.js
+++ React/ninja_app/src/Ninjas.js
@@ -5,12 +5,20 @@ class Ninjas extends Component
     console.log(this.props);
 
     //destructuring
-    const{name,age,belt} = this.props;
-    return(
-      <div className='ninja'>
-        <div>Name: {name}</div>
-        <div>Age: {age}</div>
-        <div>Belt: {belt}</div>
+    //const{name,age,belt} = this.props;
+    const{ninjas} = this.props;
+    const ninjaList = ninjas.map(ninja =>{
+      return (
+        <div className='ninja' key={ninja.id}>
+          <div>Name: {ninja.name}</div>
+          <div>Age: {ninja.age}</div>
+          <div>Belt: {ninja.belt}</div>
+        </div>
+      )
+    })
+    return(     
+      <div className="ninja-list">
+        {ninjaList}
       </div>
     )
   }
